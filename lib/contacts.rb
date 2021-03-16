005ff2fcddd59965e722ea4994830324b05adcf1
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-16 02:04:49 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -33,6 +33,11 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    binding.pry
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, value|
+        binding.pry
+
+      end
+    end
   end
 end
